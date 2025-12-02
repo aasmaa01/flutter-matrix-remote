54d1c0252d1dc16376f53a46c5c21a92c76edb7a
Asma <asmaesoltaniii@gmail.com>
2025-12-02 10:46:41 +0100

    implement BoxGrid and RemoteControl widgets for improved UI layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,23 +10,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MatrixController()
+      title: 'Remote Control Challenge',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MatrixControlScreen(),
       );
   }
 }
 //mat controller
-class MatrixController extends StatefulWidget{
-  const MatrixController({super.key})
+class MatrixControlScreen extends StatefulWidget{
+  const MatrixControlScreen({super.key});
 
-  //hadi te3 ai 
   @override
-  State<MatrixController> createState() => _MatrixControllerState();
+  State<MatrixControlScreen> createState() => _MatrixControlScreenState();
 
 
 }
-class _MatrixControllerState extends State<MatrixController>{
-  int selectedIndex=0 ;//ndiroh m 0 l 24
-  //or 
+class _MatrixControlScreenState extends State<MatrixControlScreen>{
+  //int selectedIndex=0 ;
+   
   int selectedRow = 0; // 0 to 4
   int selectedCol = 0; // 0 to 4
 
@@ -60,12 +62,168 @@ class _MatrixControllerState extends State<MatrixController>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        Ch(),
+      appBar: AppBar(
+        title: const Text('Remote control Challenge !'),
       ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BoxGrid(
+              selectedRow: selectedRow,
+              selectedCol: selectedCol,
+            ),
+            const SizedBox(width: 40),
+            RemoteControl(
+              onUp: moveUp,
+              onDown: moveDown,
+              onLeft: moveLeft,
+              onRight: moveRight,
+            ),
+          ],
+        )
+      )
     );
   }
 
 }
 
 
+// BOXES WIDGET - Displays the 5x5 grid
+class BoxGrid extends StatelessWidget {
+  final int selectedRow;
+  final int selectedCol;
+
+  const BoxGrid({
+    super.key,
+    required this.selectedRow,
+    required this.selectedCol,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey, width: 2),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: List.generate(5, (row) {
+          return Row(
+            mainAxisSize: MainAxisSize.min,
+            children: List.generate(5, (col) {
+              // Check if this box is selected
+              bool isSelected = (row == selectedRow && col == selectedCol);
+              
+              return Container(
+                width: 60,
+                height: 60,
+                margin: const EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  color: isSelected ? Colors.blue : Colors.grey[300],
+                  border: Border.all(
+                    color: isSelected ? Colors.blue[900]! : Colors.grey,
+                    width: isSelected ? 3 : 1,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              );
+            }),
+          );
+        }),
+      ),
+    );
+  }
+}
+// REMOTE WIDGET - Displays the directional buttons
+class RemoteControl extends StatelessWidget {
+  final VoidCallback onUp;
+  final VoidCallback onDown;
+  final VoidCallback onLeft;
+  final VoidCallback onRight;
+
+  const RemoteControl({
+    super.key,
+    required this.onUp,
+    required this.onDown,
+    required this.onLeft,
+    required this.onRight,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey, width: 2),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // UP button
+          IconButton(
+            onPressed: onUp,
+            icon: const Icon(Icons.arrow_upward),
+            iconSize: 40,
+            style: IconButton.styleFrom(
+              backgroundColor: Colors.grey[800],
+              foregroundColor: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 8),
+          // LEFT, CENTER, RIGHT row
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // LEFT button
+              IconButton(
+                onPressed: onLeft,
+                icon: const Icon(Icons.arrow_back),
+                iconSize: 40,
+                style: IconButton.styleFrom(
+                  backgroundColor: Colors.grey[800],
+                  foregroundColor: Colors.white,
+                ),
+              ),
+              const SizedBox(width: 8),
+              // CENTER (non-functional placeholder)
+              Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              const SizedBox(width: 8),
+              // RIGHT button
+              IconButton(
+                onPressed: onRight,
+                icon: const Icon(Icons.arrow_forward),
+                iconSize: 40,
+                style: IconButton.styleFrom(
+                  backgroundColor: Colors.grey[800],
+                  foregroundColor: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 8),
+          // DOWN button
+          IconButton(
+            onPressed: onDown,
+            icon: const Icon(Icons.arrow_downward),
+            iconSize: 40,
+            style: IconButton.styleFrom(
+              backgroundColor: Colors.grey[800],
+              foregroundColor: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
